faea47997888017551d38e829f5829125a46e126
Tanja Olsson <tanja.olsson@skola.botkyrka.sem>
2020-11-11 14:13:08 +0100

    uppgift 4

--- 4/script.js
+++ 4/script.js
@@ -1,9 +1,18 @@
 // 1. Skapa en variabel som innehåller en array med fyra strängar, t.ex. fyra namn på personer.
 
+const nånting = ["jag", "är", "trött", "idag"]
+
+
 // 2. Logga ut arrayen.
 
+console.log(nånting)
+
 // 3. Byt ut det andra elementet till något annat.
+
+nånting[1] = "äärrrr"
+console.log(nånting)
 
 // 4. Logga ut arrayen igen.
 
 // 5. Logga ut det sista elementet i arrayen.
+console.log(nånting[3])
